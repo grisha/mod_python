b9803ea6a8a475059077a5d3b1b20cf829b40414
Grisha Trubetskoy <grisha@apache.org>
2002-10-08 21:38:55 +0000

    Major changes to the test script, which now is more or less organized but still needs multitudes of documentation and additional tests. Also added a httpdconf module as part of the test suite - it generates Httpd configs similar to the way HTMLgen works. Right now its very bare, but could eventually grow into something useful.

--- test/htdocs/tests.py
+++ test/htdocs/tests.py
@@ -1,3 +1,59 @@
+ # ====================================================================
+ # The Apache Software License, Version 1.1
+ #
+ # Copyright (c) 2000-2002 The Apache Software Foundation.  All rights
+ # reserved.
+ #
+ # Redistribution and use in source and binary forms, with or without
+ # modification, are permitted provided that the following conditions
+ # are met:
+ #
+ # 1. Redistributions of source code must retain the above copyright
+ #    notice, this list of conditions and the following disclaimer.
+ #
+ # 2. Redistributions in binary form must reproduce the above copyright
+ #    notice, this list of conditions and the following disclaimer in
+ #    the documentation and/or other materials provided with the
+ #    distribution.
+ #
+ # 3. The end-user documentation included with the redistribution,
+ #    if any, must include the following acknowledgment:
+ #       "This product includes software developed by the
+ #        Apache Software Foundation (http://www.apache.org/)."
+ #    Alternately, this acknowledgment may appear in the software itself,
+ #    if and wherever such third-party acknowledgments normally appear.
+ #
+ # 4. The names "Apache" and "Apache Software Foundation" must
+ #    not be used to endorse or promote products derived from this
+ #    software without prior written permission. For written
+ #    permission, please contact apache@apache.org.
+ #
+ # 5. Products derived from this software may not be called "Apache",
+ #    "mod_python", or "modpython", nor may these terms appear in their
+ #    name, without prior written permission of the Apache Software
+ #    Foundation.
+ #
+ # THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ # OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ # DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ # ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ # SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ # LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ # USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ # SUCH DAMAGE.
+ # ====================================================================
+ #
+ # This software consists of voluntary contributions made by many
+ # individuals on behalf of the Apache Software Foundation.  For more
+ # information on the Apache Software Foundation, please see
+ # <http://www.apache.org/>.
+ #
+ # $Id: tests.py,v 1.10 2002/10/08 21:38:55 grisha Exp $
+ #
 
 # mod_python tests
 
@@ -114,8 +170,8 @@ class InternalTestCase(unittest.TestCase):
             self.fail("req.proto_num doesn't match req.protocol")
 
         log("    req.hostname: %s" % `req.hostname`)
-        if req.hostname != "127.0.0.1":
-            self.fail("req.hostname isn't '127.0.0.1'")
+        if req.hostname != "test_internal":
+            self.fail("req.hostname isn't 'test_internal'")
 
         log("    req.request_time: %s" % `req.request_time`)
         if (time.time() - req.request_time) > 2:
@@ -167,8 +223,8 @@ class InternalTestCase(unittest.TestCase):
             self.fail("req.mtime should be 0")
         
         log("    req.chunked: %s" % `req.chunked`)
-        if req.chunked != 0:
-            self.fail("req.chunked should be 0")
+        if req.chunked != 1:
+            self.fail("req.chunked should be 1")
             
         log("    req.range: %s" % `req.range`)
         if req.range:
@@ -201,9 +257,9 @@ class InternalTestCase(unittest.TestCase):
         if req.expecting_100 != 0:
             self.fail("req.expecting_100 should be 0")
 
-        log("    req.headers_int: %s" % `req.headers_in`)
-        if req.headers_in["User-agent"][:13].lower() != "python-urllib":
-            self.fail("The 'user-agnet' header should begin with 'Python-urllib'")
+        log("    req.headers_in: %s" % `req.headers_in`) 
+        if req.headers_in["Host"][:13].lower() != "test_internal":
+            self.fail("The 'Host' header should begin with 'test_internal'")
             
         log("    req.headers_out: %s" % `req.headers_out`)
         if ((not req.headers_out.has_key("content-length")) or
