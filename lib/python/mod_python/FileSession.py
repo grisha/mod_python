ea5115212d9f1e91a5248481c255fa63efa61108
nlehuen <nlehuen>
2005-04-28 07:49:02 +0000

    Latest version of FileSession by Jim Gallacher. This one does the session cleanup after the request takes place, thanks to register_cleanup, and uses a grace period to prevent valid sessions from being erased.

--- lib/python/mod_python/FileSession.py
+++ lib/python/mod_python/FileSession.py
@@ -31,71 +31,50 @@ tempdir = tempfile.gettempdir()
 
 # Credits : this was initially contributed by dharana <dharana@dharana.net>
 class FileSession(Session.BaseSession):
-    def __init__(self, req, sid=0, secret=None,
-                timeout=0, lock=1,
-                fast_cleanup=True, verify_cleanup=False):
+
+    def __init__(self, req, sid=0, secret=None, timeout=0, lock=1,
+                fast_cleanup=True, verify_cleanup=False, grace_period=240):
         
         opts = req.get_options()
         self._sessdir = opts.get('FileSessionDir',tempdir)
         
         self._fast_cleanup = fast_cleanup
         self._verify_cleanup = verify_cleanup
+        self._grace_period = grace_period
         if timeout:
-            self._fast_timeout = timeout
+            self._cleanup_timeout = timeout
         else:
-            self._fast_timeout = Session.DFT_TIMEOUT
+            self._cleanup_timeout = Session.DFT_TIMEOUT
         
         Session.BaseSession.__init__(self, req, sid=sid, secret=secret,
             timeout=timeout, lock=lock)
 
     def do_cleanup(self):
-        # WARNING - this method does no file locking.
-        # Problems may lurk here.
-        self._req.log_error('Sessions cleanup (fast=%s, verify=%s) ...' 
-            % (self._fast_cleanup,self._verify_cleanup),
-            apache.APLOG_NOTICE)
+        data = {'req':self._req,
+                'sessdir':self._sessdir,
+                'fast_cleanup':self._fast_cleanup, 
+                'verify_cleanup':self._verify_cleanup, 
+                'timeout':self._cleanup_timeout,
+                'grace_period':self._grace_period}
 
-        for f in os.listdir(self._sessdir):
-            if not f.startswith('mp_sess_'):
-                continue
-            
-            try:
-                filename = os.path.join(self._sessdir, f)
-                if self._fast_cleanup:
-                    mtime = os.stat(filename).st_mtime
-                    if time.time() - mtime < self._fast_timeout:
-                        continue
-            
-                # FIXME - What happens if another process saved it's
-                # session data to filename after the above test but
-                # before we have a chance to deleted the file below?
-                # WARNING - We may be deleting a completely valid session file.
-                # Oops. Sorry.
-                if self._fast_cleanup and not self._verify_cleanup:        
-                    os.unlink(filename)
-                else:
-                    try:
-                        fp = file(filename)
-                        dict = cPickle.load(fp)
-                        if (time.time() - dict['_accessed']) > dict['_timeout']:
-                            os.unlink(filename)
-                    finally:
-                        fp.close()
-            except:
-                s = cStringIO.StringIO()
-                traceback.print_exc(file=s)
-                s = s.getvalue()
-                self._req.log_error('Error while cleaning up the sessions : %s'%s)
+        self._req.register_cleanup(filesession_cleanup, data)
+        self._req.log_error("FileSession: registered filesession cleanup.",
+                            apache.APLOG_NOTICE)
 
     def do_load(self):
         self.lock_file()
         try:
             try:
-                # again, is there a more pythonic way of doing this check?
                 # TODO : why does this load fails sometimes with an EOFError ?
-                fp = file(os.path.join(self._sessdir, 'mp_sess_%s' % self._sid))
+                filename = os.path.join(self._sessdir, 'mp_sess_%s' % self._sid)
+                fp = file(filename)
                 try:
                     data = cPickle.load(fp)
+                    if (time.time() - data["_accessed"]) <= data["_timeout"]:
+                        # Change the file access time to the current time so the 
+                        # cleanup does not delete this file before the request
+                        # can save it's session data
+                        os.utime(filename,None)
                     return data
                 finally:
                     fp.close()
@@ -147,3 +126,82 @@ class FileSession(Session.BaseSession):
         if self._locked and not self._lock:
             _apache._global_unlock(self._req.server, self._sid)
             self._locked = 0
+
+
+def filesession_cleanup(data):
+    # There is a small chance that a the cleanup for a given session file
+    # may occur at the exact time that the session is being accessed by
+    # another request. It is possible under certain circumstances for that
+    # session file to be saved in another request only to immediately deleted
+    # by the cleanup. To avoid this race condition, a session is allowed a 
+    # grace_period before it is considered for deletion by the cleanup. 
+    # As long as the grace_period is longer that the time it takes to complete
+    # the request (which should normally be less than 1 second), the session will
+    # not be mistakenly deleted by the cleanup. By doing this we also avoid the
+    # need to lock individual sessions and bypass any potential deadlock
+    # situations.
+   
+    req = data['req']
+    sessdir = data['sessdir']
+    fast_cleanup = data['fast_cleanup']
+    verify_cleanup = data['verify_cleanup']
+    timeout = data['timeout']
+    grace_period = data['grace_period']
+
+    req.log_error('Sessions cleanup (fast=%s, verify=%s) ...'
+        % (fast_cleanup,verify_cleanup),
+        apache.APLOG_NOTICE)
+
+    lockfile = os.path.join(sessdir,'.mp_sess.lck')
+    try:
+        lockfp = os.open(lockfile, os.O_CREAT | os.O_EXCL | os.O_WRONLY, 0660) 
+    except:
+        req.log_error('filesession_cleanup: another process is already running.') 
+        return
+
+    try:
+        start_time = time.time()
+        filelist =  os.listdir(sessdir)
+        count = 0
+        i = 0
+        for f in filelist:
+            if not f.startswith('mp_sess_'):
+                continue
+            try:
+                filename = os.path.join(sessdir, f)
+                if fast_cleanup:
+                    accessed = os.stat(filename).st_mtime
+                    if time.time() - accessed < (timeout + grace_period):
+                        continue
+
+                if fast_cleanup and not verify_cleanup:        
+                    delete_session = True
+                else:
+                    try:
+                        fp = file(filename)
+                        dict = cPickle.load(fp)
+                        if (time.time() - dict['_accessed']) > (dict['_timeout'] + grace_period):
+                            delete_session = True
+                        else:
+                            delete_session = False
+                    finally:
+                        fp.close()
+                if delete_session:
+                    os.unlink(filename)
+                    count += 1
+            except:
+                s = cStringIO.StringIO()
+                traceback.print_exc(file=s)
+                s = s.getvalue()
+                req.log_error('Error while cleaning up the sessions : %s' % s)
+
+        elapsed_time = time.time() - start_time
+        req.log_error('filesession cleanup: %d of %d in %.4f seconds' % (count,len(filelist), elapsed_time))
+   
+        try:
+            os.unlink(lockfile)
+        except:
+            pass
+
+    finally:
+        os.close(lockfp)
