a34749b3dd2d05a4aa49c9f08574f34ea6460a6e
jgallacher <jgallacher>
2005-08-31 20:07:01 +0000

    Updated version info to reflect documentation changes.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
 #define MPV_PATCH 0
-#define MPV_BUILD 20050828
-#define MPV_STRING "3.2.0-dev-20050828"
+#define MPV_BUILD 20050831
+#define MPV_STRING "3.2.0-dev-20050831"
