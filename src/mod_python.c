24be83336b853cb508135130137ca16ceab9bc58
nlehuen <nlehuen>
2005-09-13 20:35:57 +0000

    Fixed : APR_HAS_THREADS ends with an 'S'. Reintroduced the calls to PyEval_AcquireLock and PyEval_ReleaseLock as they are required if threading is enabled. Removed two debugging log entries. Added the conditional commenting of the LockFile directive in the LockFile class.

--- src/mod_python.c
+++ src/mod_python.c
@@ -31,7 +31,7 @@
  * (In a Python dictionary) */
 static PyObject * interpreters = NULL;
 
-#ifdef APR_HAS_THREAD
+#if APR_HAS_THREADS
 static apr_thread_mutex_t* interpreters_lock = 0;
 #endif
 
@@ -129,8 +129,11 @@ static interpreterdata *get_interpreter(const char *name, server_rec *srv)
     if (! name)
         name = MAIN_INTERPRETER;
 
-#ifdef APR_HAS_THREAD
+#if APR_HAS_THREADS
     apr_thread_mutex_lock(interpreters_lock);
+#endif
+#ifdef WITH_THREAD
+    PyEval_AcquireLock();
 #endif
 
     if (!interpreters) {
@@ -157,7 +160,10 @@ static interpreterdata *get_interpreter(const char *name, server_rec *srv)
         idata = (interpreterdata *)PyCObject_AsVoidPtr(p);
     }
 
-#ifdef APR_HAS_THREAD
+#ifdef WITH_THREAD
+    PyEval_ReleaseLock();
+#endif
+#if APR_HAS_THREADS
     apr_thread_mutex_unlock(interpreters_lock);
 #endif
 
@@ -513,10 +519,8 @@ static int python_init(apr_pool_t *p, apr_pool_t *ptemp,
         /* initialze the interpreter */
         Py_Initialize();
 
-#ifdef APR_HAS_THREAD
-        ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, s, "just before");
+#if APR_HAS_THREADS
         apr_thread_mutex_create(&interpreters_lock, APR_THREAD_MUTEX_UNNESTED, p);
-        ap_log_error(APLOG_MARK, APLOG_NOERRNO|APLOG_ERR, 0, s, "just after");
 #endif
 #ifdef WITH_THREAD
         /* create and acquire the interpreter lock */
