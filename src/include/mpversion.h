8b0a56bce4e149e7420c5781607415663b6698a0
jgallacher <jgallacher>
2005-10-28 16:17:01 +0000

    Bumped version to 3.2.4-dev-20051028

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
-#define MPV_PATCH 3
-#define MPV_BUILD 20051022
-#define MPV_STRING "3.2.3b"
+#define MPV_PATCH 4
+#define MPV_BUILD 20051028
+#define MPV_STRING "3.2.4-dev-20051028"
