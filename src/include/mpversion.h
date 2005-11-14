cc857a8e237ec883269bf9a81f1e662b142f46bb
jgallacher <jgallacher>
2005-11-14 21:30:54 +0000

    Preparing to tag 3.2.5b.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
 #define MPV_PATCH 5
-#define MPV_BUILD 20051112
-#define MPV_STRING "3.2.5-dev-20051112"
+#define MPV_BUILD 20051114
+#define MPV_STRING "3.2.5b"
