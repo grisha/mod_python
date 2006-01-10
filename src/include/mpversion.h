3d07b2d45e2259fc09d8fecd1f93c69e4bd25f76
jgallacher <jgallacher>
2006-01-10 22:40:09 +0000

    Fixed PythonAutoReload directive problem. Ref MODPYTHON-106

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
 #define MPV_PATCH 6
-#define MPV_BUILD 20051229
-#define MPV_STRING "3.2.6-dev-20051229"
+#define MPV_BUILD 20060110
+#define MPV_STRING "3.2.6-dev-20060110"
