062a1301700ad4949407b2d749e8e58772856e62
jgallacher <jgallacher>
2006-02-19 19:51:17 +0000

    Added check for the validity of the session id. This will fix a potential directory traversal attack in FileSession. Ref MODPYTHON-135

--- lib/python/mod_python/__init__.py
+++ lib/python/mod_python/__init__.py
@@ -20,5 +20,5 @@
 __all__ = ["apache", "cgihandler", "psp",
            "publisher", "util", "python22"]
 
-version = "3.2.7"
+version = "3.2.8"
 
--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
-#define MPV_PATCH 7
-#define MPV_BUILD 20060202
-#define MPV_STRING "3.2.7"
+#define MPV_PATCH 8
+#define MPV_BUILD 20060219
+#define MPV_STRING "3.2.8"
