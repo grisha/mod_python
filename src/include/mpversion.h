503e47b23deede56402ec49a8a5e77d606c0eb77
jgallacher <jgallacher>
2006-03-05 19:12:22 +0000

    Bumped version for 3.2.x branch.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
-#define MPV_PATCH 8
-#define MPV_BUILD 20060219
-#define MPV_STRING "3.2.8"
+#define MPV_PATCH 9
+#define MPV_BUILD 20060305
+#define MPV_STRING "3.2.9-unreleased-20060305"
