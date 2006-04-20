97fb28a73737413c4f53ad31acfcd4f22e6c31ef
grahamd <grahamd>
2006-04-20 12:04:23 +0000

    Debian backported ap_regex_t to Apache 2.0 and thus made official version checking break for this feature. Use explicit check for AP_REG_EXTENDED in addition to version check. (MODPYTHON-63) (MODPYTHON-126)

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 3
 #define MPV_PATCH 0
-#define MPV_BUILD 20060419
-#define MPV_STRING "3.3.0-dev-20060419"
+#define MPV_BUILD 20060420
+#define MPV_STRING "3.3.0-dev-20060420"
