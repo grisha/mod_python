94d0eb2e845eb5e7379971b9ad998ca5d076b27b
jgallacher <jgallacher>
2006-06-23 03:36:05 +0000

    Bumped version strings.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 2
 #define MPV_PATCH 9
-#define MPV_BUILD 20060413
-#define MPV_STRING "3.2.9-unreleased-20060413"
+#define MPV_BUILD 20060622
+#define MPV_STRING "3.2.9-rc1"
