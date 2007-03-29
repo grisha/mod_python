44042d0838f3ef5e01ef766d20ac35654bb6ff8d
jgallacher <jgallacher>
2007-03-29 20:18:18 +0000

    Added links in Request member docs to server object and connection object.

--- src/include/mpversion.h
+++ src/include/mpversion.h
@@ -1,5 +1,5 @@
 #define MPV_MAJOR 3
 #define MPV_MINOR 3
-#define MPV_PATCH 1
-#define MPV_BUILD 20070129
-#define MPV_STRING "3.3.1"
+#define MPV_PATCH 2
+#define MPV_BUILD 20070329
+#define MPV_STRING "3.3.2-dev-20070329"
