7ce5fd69b1ac9deea26c369f60e651b9792f6916
grahamd <grahamd>
2008-07-15 13:01:38 +0000

    Fix coding problem uncovered by apr 1.3.2. See MODPYTHON-249.

--- src/connobject.c
+++ src/connobject.c
@@ -139,7 +139,7 @@ static PyObject * _conn_read(conn_rec *c, ap_input_mode_t mode, long len)
     bytes_read = 0;
 
     while ((bytes_read < len || len == 0) &&
-           !(b == APR_BRIGADE_SENTINEL(b) ||
+           !(b == APR_BRIGADE_SENTINEL(bb) ||
              APR_BUCKET_IS_EOS(b) || APR_BUCKET_IS_FLUSH(b))) {
 
         const char *data;
