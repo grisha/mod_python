2b16c1653be29167ccd34bde169203bdfe8f96f7
Grisha Trubetskoy <grisha@modpython.org>
2013-09-25 13:07:59 -0400

    Remove --dirty argument from git describe, it only causes confution in tests

--- dist/version.sh
+++ dist/version.sh
@@ -5,6 +5,6 @@ MPV_PATH="`dirname $0`/../src/include/mp_version.h"
 MAJ=`awk '/MP_VERSION_MAJOR/ {print $3}' $MPV_PATH`
 MIN=`awk '/MP_VERSION_MINOR/ {print $3}' $MPV_PATH`
 PCH=`awk '/MP_VERSION_PATCH/ {print $3}' $MPV_PATH`
-GIT=`git describe --dirty --always`
+GIT=`git describe --always`
 
 echo $MAJ.$MIN.$PCH-$GIT
