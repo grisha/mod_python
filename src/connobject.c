d269c883fabc18caaf1bb3945eddf6f1472eeadc
shreyas202 <72181121+shreyas202@users.noreply.github.com>
2020-10-02 09:17:15 +0530

    Update connobject.c

--- src/connobject.c
+++ src/connobject.c
@@ -14,7 +14,7 @@
  * implied.  See the License for the specific language governing
  * permissions and limitations under the License.
  *
- * Originally developed by Gregory Trubetskoy.
+ * Originally developed by Gregory Trubetskoy.hellow friends
  *
  *
  * connobject.c
